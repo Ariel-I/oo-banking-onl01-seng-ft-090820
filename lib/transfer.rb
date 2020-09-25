a930147997ba69696e4ad86af5f33332cf456657
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-25 20:37:38 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,13 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :transfer, :sender, :receiver, :status, :transfer_amount
+  
+  def initialize(transfer)
+    @transfer = transfer
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @transfer_amount = transfer_amount
+  end 
+  
 end
