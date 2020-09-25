c19e3fc4e841988dedc802b5082d470312acae97
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-25 21:19:55 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,8 @@ class Transfer
   end 
   
   def execute_transaction
-    if valid? sender.balance > amount 
+    if valid? sender.balance > amount && self.status = "pending"
+      
   end
 
   def reverse_transfer
