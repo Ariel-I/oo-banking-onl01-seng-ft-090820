5a1914bbd786690a6e37ef2abbd596accc43ccfb
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-25 22:04:46 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,11 +1,10 @@
 class BankAccount
 
-attr_accessor :bank_account, :balance, :status
+attr_accessor :balance, :status
 attr_reader :name 
 
 def initialize(name)
   @name = name
-  @bank_account = bank_account
   @balance = 1000
   @status = "open"
 end
