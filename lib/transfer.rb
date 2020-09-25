72ff1127221865ca1db560550094a979f47e2b59
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-25 21:53:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,6 +20,7 @@ class Transfer
       receiver.deposit(@amount)
       @status = "complete"
     else
+      sender.balance < amount 
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
